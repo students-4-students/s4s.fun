c7f4a79386234486993762ed9bd59230175d3855
Johnny Kalajdzic <johnny.kalajdzic@epfl.ch>
2024-08-24 23:17:00 +0200

    update schedule for CMS

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -9,6 +9,7 @@ const typeNames = {
     c: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"></svg> Cours',
     e: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></svg> Exercices',
     p: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" /></svg> Présentation',
+    r: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" /></svg> Questions',
     w: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></svg> Activités',
     b: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></svg> Pause'
 };
@@ -24,7 +25,7 @@ const sections = [
     },
     {
         name: 'CMS',
-        courses: ["cms-ceremony", "cms-workshop_learn2study", "ws", "snack", "cms-pr", "cms-pg", "cms-al", "cms-ga", "cms-an"],
+        courses: ["cms-ceremony", "cms-workshop_calculs", "ws", "snack", "cms-pr", "cms-raq", "cms-pg", "cms-al", "cms-ga", "cms-an"],
     },
     {
         name: 'Génie civil',
@@ -94,7 +95,7 @@ const COURSES = {
     },
     "snack":{
         "name": "Goûter",
-        "breaks": ["ven 30 15h30-16h", "sam 31 15h30-16h", "lun 2 15h30-16h", "mar 3 15h30-16h", "mer 4 15h30-16h"],
+        "breaks": ["ven 30 15h30-16h", "sam 31 15h30-16h", "lun 2 15h30-16h", "mar 3 15h30-16h", "mer 4 15h30-16h", "jeu 5 15h30-16h"],
     },
     "cms-ceremony": {
         "name": "Cérémonie d'ouverture",
@@ -104,9 +105,13 @@ const COURSES = {
         "name": "Workshop apprendre à étudier",
         "activities": ["jeu 29 9h-12h"],
     },
-    "cms-workshop_learn2study": {
-        "name": "Workshop apprendre à étudier",
+    "cms-workshop_calculs": {
+        "name": "Workshop rappels calculs algébriques",
         "activities": ["jeu 29 10h-12h"],
+    },
+    "cms-raq": {
+        "name": "R.A.Q",
+        "raq": ["mer 4 13h30-15h30"],
     },
     "ws": {
         "name": "Workshops",
@@ -213,42 +218,25 @@ function findTimeSlot(hours, search_day, search_time) {
 }
 
 function findCourse(section, search_day, search_time) {
-    for (let c in section.courses) {
-        const course = COURSES[section.courses[c]];
-        if (findTimeSlot(course.courses, search_day, search_time)) {
-            return {
-                name: course.name,
-                code: section.courses[c].split("-").reverse()[0],
-                type: "c"
-            };
-        }
-        if (findTimeSlot(course.exercices, search_day, search_time)) {
-            return {
-                name: course.name,
-                code: section.courses[c].split("-").reverse()[0],
-                type: "e"
-            };
-        }
-        if (findTimeSlot(course.presentations, search_day, search_time)) {
-            return {
-                name: course.name,
-                code: section.courses[c].split("-").reverse()[0],
-                type: "p"
-            };
-        }
-        if (findTimeSlot(course.activities, search_day, search_time)) {
-            return {
-                name: course.name,
-                code: section.courses[c].split("-").reverse()[0],
-                type: "w"
-            };
-        }
-        if (findTimeSlot(course.breaks, search_day, search_time)) {
-            return {
-                name: course.name,
-                code: section.courses[c].split("-").reverse()[0],
-                type: "b"
-            };
+    const types = [
+        { key: 'courses', type: 'c' },
+        { key: 'exercices', type: 'e' },
+        { key: 'presentations', type: 'p' },
+        { key: 'activities', type: 'w' },
+        { key: 'breaks', type: 'b' },
+        { key: 'raq', type: 'r' }
+    ];
+    for (let c of section.courses) {
+        const course = COURSES[c];
+
+        for (let { key, type } of types) {
+            if (findTimeSlot(course[key], search_day, search_time)) {
+                return {
+                    name: course.name,
+                    code: c.split("-").reverse()[0],
+                    type: type
+                };
+            }
         }
     }
     return undefined;
@@ -256,14 +244,12 @@ function findCourse(section, search_day, search_time) {
 
 /**********************************************************************************************************************/
 function computeCourseHtml(section, courseIndex) {
-    console.log(courseIndex);
     const search_day = Math.floor(courseIndex / TIMES.length);
     let search_time = courseIndex % TIMES.length;
 
     let ret = findCourse(section, search_day, ALL_TIMES.indexOf(TIMES[search_time]));
 
     if (ret === undefined) {
-        console.log("An error has occurred")
         return `<div class="schedule-course" style="visibility: hidden" id="schedule-js-edit"></div>`;
     }
     return `<div class="schedule-course course-${ret.code}" id="schedule-js-edit">
