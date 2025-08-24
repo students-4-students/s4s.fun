67a70302f289a7ef35e45ee97623b4ca7376c276
AdamBEKKAR <bekkaradam05@gmail.com>
2025-08-24 18:45:08 +0200

    feat: add schedule for archi's section

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -8,6 +8,7 @@
 const typeNames = {
     c: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"></svg> Cours',
     e: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></svg> Exercices',
+    ex: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path d="M3 12l9-9 9 9v9a1 1 0 0 1-1 1h-5v-6H9v6H4a1 1 0 0 1-1-1v-9z" stroke-linecap="round" stroke-linejoin="round"/></svg> Exposition',
     p: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" /></svg> Présentation',
     r: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" /></svg> Questions',
     w: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></svg> Activités',
@@ -18,7 +19,7 @@ const typeNames = {
 const sections = [
     {
         name: 'Architecture',
-        message: 'Le programme de l’architecture sera bientôt disponible. Merci de votre patience.',
+        courses: ["opening", "ws", "snack", "archi-pt", "archi-geo", "archi-tb", "archi-m3D", "archi-expo", "archi-hat", "archi-math", "surprise"]
     },
     {
         name: 'Chimie et génie chimique',
@@ -186,6 +187,39 @@ const COURSES = {
         "name": "Programmation",
         "exercices": ["ven 29 8-10h", "mer 3 10h-12h"]
     },
+    "archi-geo": {
+        "name": "Géométrie",
+        "courses": ["jeu 28 8h15-12h"],
+        "exercices": ["jeu 28 13h45-15h30"]
+    },
+    "archi-pt": {
+        "name": "Théorie du projet",
+        "courses": ["ven 29 8h15-12h"],
+        "exercices": ["ven 29 13h45-15h30"]
+    },
+    "archi-tb": {
+        "name": "Technologie du bâtiment",
+        "courses": ["sam 30 8h15-12h"],
+        "exercices": ["sam 30 13h45-15h30"]
+    },
+    "archi-m3D": {
+        "name": "Modélisation 3D",
+        "courses": ["lun 1 8h15-12h"],
+        "exercices": ["lun 1 13h45-15h30"]
+    },
+    "archi-expo": {
+        "name": "Exposition",
+        "exposition": ["mar 2 8h15-12h", "mar 2 13h45-15h30"]
+    },
+    "archi-hat": {
+        "name": "Histoire de l'architecture et territoire",
+        "courses": ["mer 3 8h15-10h"]
+    },
+    "archi-math": {
+        "name": "Mathématique pour architecte",
+        "courses": ["mer 3 10h15-12h"],
+        "exercices": ["mer 3 13h45-15h30"]
+    },
     "surprise": {
         "name": "Surprise",
         "surprise": ["jeu 4 8h15-19h"]
@@ -218,6 +252,7 @@ function findCourse(section, search_day, search_time) {
     const types = [
         { key: 'courses', type: 'c' },
         { key: 'exercices', type: 'e' },
+        { key: 'exposition', type: 'ex' },
         { key: 'presentations', type: 'p' },
         { key: 'activities', type: 'w' },
         { key: 'breaks', type: 'b' },
